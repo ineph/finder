ecedac84c0281ac60b4fd62d844ebdcd3c9faf34
Lucas Goss <lucas.goss@addesistemas.com.br>
2021-10-01 00:13:16 -0300

    TODOs porra!

--- finder_src/main.py
+++ finder_src/main.py
@@ -5,9 +5,9 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 
-# migrar essa porra toda pra outro arquivo(/)
 def finder():
     exif = {}
+    # TODO: adicionar mais extensões (imagens, videos, musicas, texto simples)
     imagens_extensoes = ['.jpg', '.png', '.jpeg']
     pasta_alvo = env.INPUT_PATH
     pasta_destino = env.OUTPUT_PATH
@@ -15,6 +15,7 @@ def finder():
     for arquivo in os.listdir(pasta_alvo):
         arquivo_nome, arquivo_extensao = os.path.splitext(arquivo)
 
+        # TODO: navegar para pastas filhas e procurar mais arquivos recursamente
         if arquivo_extensao in imagens_extensoes:
             imagem = Image.open(f"{pasta_alvo}/{arquivo}")
             exif_arquivo = imagem._getexif()
@@ -24,7 +25,7 @@ def finder():
                 for tag, value in exif_arquivo.items():
                     if tag in TAGS:
                         exif[TAGS[tag]] = value
-
+                # TODO: adicionar parâmetros dinâmicos (modelos de cameras e tamanhos)
                 if exif["Model"] == 'GT-M2520':
                     arquivo_criador(pasta_destino, pasta_alvo, 'GT-M2520', arquivo)
 
@@ -38,13 +39,18 @@ def finder():
                 if imagem_tamanho[1] == 800 and imagem_tamanho[0] == 600:
                     arquivo_criador(pasta_destino, pasta_alvo, '600x800', arquivo)
 
+                if imagem_tamanho[0] == 1024 and imagem_tamanho[1] == 768:
+                    arquivo_criador(pasta_destino, pasta_alvo, '1024x768', arquivo)
+
 
 def arquivo_criador(pasta_destino, pasta_alvo, pasta_nome, arquivo):
     if not os.path.exists(f"{pasta_destino}/{pasta_nome}"):
         os.makedirs(f"{pasta_destino}/{pasta_nome}")
     shutil.copy2(f"{pasta_alvo}/{arquivo}", f"{pasta_destino}/{pasta_nome}/{arquivo}")
 
+
 if __name__ == '__main__':
+    # TODO: organizar estrutura e separar em arquivos (sem overengineering, caralho)
     finder()
 
 
